70c793ed436294bef3bf1cbcb33d87aedbcbc735
avan1235 <macieekprocyk@gmail.com>
2021-12-05 14:20:50 +0100

    rename helper function

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -16,7 +16,7 @@ object Day5 : AdventDay() {
 }
 
 private data class P(val x: Int, val y: Int)
-private data class Line(val from: P, val to: P) {
+private data class L(val from: P, val to: P) {
     val isHorizontal = from.x == to.x
     val isVertical = from.y == to.y
     val isDiagonal = abs(from.y - to.y) == abs(from.x - to.x)
@@ -24,13 +24,13 @@ private data class Line(val from: P, val to: P) {
 
 private fun String.toLine() = split(" -> ").map { p ->
     p.split(",").let { (x, y) -> P(x.toInt(), y.toInt()) }
-}.let { (f, t) -> Line(f, t) }
+}.let { (f, t) -> L(f, t) }
 
 private class Diagram {
     private val _m = DefaultMap<P, Int>(0)
-    val marked: Map<P, Int> get() = _m
+    val marked: Map<P, Int> = _m
 
-    fun markLine(line: Line) = with(line) {
+    fun markLine(line: L) = with(line) {
         when {
             isVertical -> (from.x directedTo to.x).map { P(it, to.y) }.forEach { _m[it] = _m[it] + 1 }
             isHorizontal -> (from.y directedTo to.y).map { P(to.x, it) }.forEach { _m[it] = _m[it] + 1 }
