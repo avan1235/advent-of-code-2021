6c76fa339a240c8f0ccc1685442fe41478876fcb
avan1235 <macieekprocyk@gmail.com>
2021-12-07 14:13:49 +0100

    simplify day 7

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -2,21 +2,27 @@ import kotlin.math.abs
 import kotlin.math.ceil
 import kotlin.math.floor
 
-object Day7 : AdventDay() {
+object Day7 : AdventDay(readFromStdIn = true) {
     override fun solve() {
         val positions = reads<String>()?.singleOrNull()
             ?.let { line -> line.split(",").map { it.value<Int>() } } ?: return
 
-        val median = positions.median()
-        val avg = positions.average().let { if (it > median) floor(it) else ceil(it) }.toInt()
+        positions.median()
+            .let { positions.absCost(it) }
+            .printIt()
 
-        positions.distance(median) { from, to -> abs(from - to) }.printIt()
-        (avg directedTo median).minOf {
-            positions.distance(it) { from, to -> abs(from - to) * (abs(from - to) + 1) / 2 }
-        }.printIt()
+        positions.average()
+            .let { sequenceOf(floor(it), ceil(it)) }
+            .map { it.toInt() }
+            .minOf { positions.incrCost(it) }
+            .printIt()
     }
 }
 
+private fun List<Int>.absCost(pos: Int) = distance(pos) { from, to -> abs(from - to) }
+
+private fun List<Int>.incrCost(pos: Int) = distance(pos) { from, to -> abs(from - to) * (abs(from - to) + 1) / 2 }
+
 private fun List<Int>.distance(from: Int, by: (Int, Int) -> Int) = sumOf { by(from, it) }
 
-private fun Iterable<Int>.median() = sorted().run { (this[(size - 1) / 2] + this[size / 2]) / 2 }
+private fun List<Int>.median() = sorted().run { (this[(size - 1) / 2] + this[size / 2]) / 2 }
