dfeedc55154ed3b3a6b4bf7261a917992b7ab099
avan1235 <macieekprocyk@gmail.com>
2021-12-09 08:14:24 +0100

    Add Day 9

---
+++ src/main/kotlin/Day9.kt
@@ -0,0 +1,50 @@
+object Day9 : AdventDay() {
+    override fun solve() {
+        val map = reads<String>()?.toMap() ?: return
+
+        map.indices.asSequence()
+            .filter { p -> map.neighbours(of = p).all { map[it] > map[p] } }
+            .sumOf { map[it] + 1 }
+            .printIt()
+
+        map.indices.asSequence()
+            .map { p -> map.dfs(p) { from, to -> from < to && to != 9 } }
+            .distinct().map { it.size }
+            .sortedDescending().take(3)
+            .fold(1, Int::times)
+            .printIt()
+    }
+}
+
+private fun List<String>.toMap() = Map(map { line -> line.map { it.digitToInt() } })
+
+private data class Pos(val x: Int, val y: Int)
+
+private data class Map<V>(val heights: List<List<V>>) {
+
+    val indices = heights.flatMapIndexed { y, row -> row.indices.map { Pos(it, y) } }
+
+    operator fun get(p: Pos): V = with(p) { heights[y][x] }
+
+    fun neighbours(of: Pos) = with(of) {
+        sequenceOf(
+            Pos(x + 1, y), Pos(x - 1, y),
+            Pos(x, y + 1), Pos(x, y - 1)
+        ).filter { it.isValid() }
+    }
+
+    fun dfs(start: Pos, visit: (V, V) -> Boolean): Set<Pos> {
+        val visited = mutableSetOf<Pos>()
+        val queue = ArrayDeque<Pos>()
+        tailrec fun go(from: Pos) {
+            visited += from
+            neighbours(from).filterNot { it in visited }
+                .filter { visit(this[from], this[it]) }
+                .forEach { queue += it }
+            go(queue.removeLastOrNull() ?: return)
+        }
+        return visited.also { go(start) }
+    }
+
+    private fun Pos.isValid() = y in heights.indices && x in heights[y].indices
+}
