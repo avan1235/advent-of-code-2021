e20d9016996f8da17ac3fdacb5bb382af32b9cf4
avan1235 <macieekprocyk@gmail.com>
2021-12-15 20:39:30 +0100

    Clean code

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -18,13 +18,11 @@ private fun List<String>.toWeightedGraph(times: Int): WeightedGraph = map { line
   val nd = n * times
 
   LazyDefaultMap<N, MutableList<E>>(::mutableListOf).also { adj ->
-    fun isOnMap(x: Int, y: Int) = x in 0 until md && y in 0 until md
     for (x in 0 until md) for (y in 0 until nd)
-      for ((xc, yc) in listOf(1 to 0, -1 to 0, 0 to 1, 0 to -1)) {
-        if (!isOnMap(x + xc, y + yc)) continue
-        val tx = x + xc
-        val ty = y + yc
-        adj[x o y] += E(tx o ty, (data[ty % n][tx % m] + (ty / n) + (tx / m) - 1) % 9 + 1)
+      for ((tx, ty) in listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1)) {
+        if (tx !in 0 until md || ty !in 0 until nd) continue
+        val extra = (ty / n) + (tx / m)
+        adj[x `#` y] += E(tx `#` ty, (data[ty % n][tx % m] + extra - 1) % 9 + 1)
       }
   }.let { WeightedGraph(md, nd, it) }
 }
@@ -32,27 +30,27 @@ private fun List<String>.toWeightedGraph(times: Int): WeightedGraph = map { line
 private data class N(val x: Int, val y: Int)
 private data class E(val to: N, val w: Int)
 
-private infix fun Int.o(v: Int) = N(this, v)
+private infix fun Int.`#`(v: Int) = N(this, v)
 
 private class WeightedGraph(val m: Int, val n: Int, private val adj: Map<N, List<E>>) {
 
-  fun shortestPathLength(source: N = N(0, 0), dest: N = N(m - 1, n - 1)): Long {
+  fun shortestPathLength(source: N = 0 `#` 0, dest: N = m - 1 `#` n - 1): Long {
     data class QN(val n: N, val dist: Long)
 
     val dist = DefaultMap<N, Long>(0)
     val queue = PriorityQueue(compareBy(QN::dist))
     adj.keys.forEach { v ->
       if (v != source) dist[v] = Long.MAX_VALUE
-      queue.add(QN(v, dist[v]))
+      queue += QN(v, dist[v])
     }
 
     while (queue.isNotEmpty()) {
       val u = queue.remove()
-      adj[u.n]?.forEach { edge ->
+      adj[u.n]?.forEach neigh@{ edge ->
         val alt = dist[u.n] + edge.w
-        if (alt >= dist[edge.to]) return@forEach
+        if (alt >= dist[edge.to]) return@neigh
         dist[edge.to] = alt
-        queue.add(QN(edge.to, alt))
+        queue += QN(edge.to, alt)
       }
     }
     return dist[dest]
